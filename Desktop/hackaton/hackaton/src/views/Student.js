da6a8fa1e51737e18f82c52d6a186bd926302555
Hải Nam <hainam@hnam.local>
2020-11-04 18:30:40 +0700

    fontend

---
+++ Desktop/hackaton/hackaton/src/views/Student.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { } from '@material-ui/core'
+
+class inside extends Component {
+    render() {
+        return <div style={{
+            color: 'cyan'
+        }}>
+        </div>
+    }
+}
+
+export default inside;
